4c629941a267a0586b4d839f1db4fdea68fa677e
roudique <roudique@gmail.com>
2017-02-15 12:42:36 -0500

    Add Pod

---
+++ Podfile
@@ -0,0 +1,7 @@
+target :’Eaterator’ do
+platform :ios, ‘10.0’
+use_frameworks!
+
+pod 'Alamofire', '~> 4.3'
+
+end
---
+++ Pods/Target Support Files/Pods-Eaterator/Pods-Eaterator-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Eaterator : NSObject
+@end
+@implementation PodsDummy_Pods_Eaterator
+@end
